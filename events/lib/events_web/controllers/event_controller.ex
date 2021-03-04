3e9ae8d42756d9ba1c2ac9b41c24d2c9f32edd0f
HW04 <hw04@guest.guest>
2021-03-04 23:36:33 +0000

    db stuff

---
+++ events/lib/events_web/controllers/event_controller.ex
@@ -0,0 +1,62 @@
+defmodule EventsWeb.EventController do
+  use EventsWeb, :controller
+
+  alias Events.Users
+  alias Events.Users.Event
+
+  def index(conn, _params) do
+    events = Users.list_events()
+    render(conn, "index.html", events: events)
+  end
+
+  def new(conn, _params) do
+    changeset = Users.change_event(%Event{})
+    render(conn, "new.html", changeset: changeset)
+  end
+
+  def create(conn, %{"event" => event_params}) do
+    case Users.create_event(event_params) do
+      {:ok, event} ->
+        conn
+        |> put_flash(:info, "Event created successfully.")
+        |> redirect(to: Routes.event_path(conn, :show, event))
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
+
+  def show(conn, %{"id" => id}) do
+    event = Users.get_event!(id)
+    render(conn, "show.html", event: event)
+  end
+
+  def edit(conn, %{"id" => id}) do
+    event = Users.get_event!(id)
+    changeset = Users.change_event(event)
+    render(conn, "edit.html", event: event, changeset: changeset)
+  end
+
+  def update(conn, %{"id" => id, "event" => event_params}) do
+    event = Users.get_event!(id)
+
+    case Users.update_event(event, event_params) do
+      {:ok, event} ->
+        conn
+        |> put_flash(:info, "Event updated successfully.")
+        |> redirect(to: Routes.event_path(conn, :show, event))
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, "edit.html", event: event, changeset: changeset)
+    end
+  end
+
+  def delete(conn, %{"id" => id}) do
+    event = Users.get_event!(id)
+    {:ok, _event} = Users.delete_event(event)
+
+    conn
+    |> put_flash(:info, "Event deleted successfully.")
+    |> redirect(to: Routes.event_path(conn, :index))
+  end
+end
