f83975f36d737227d024c31e8bf9542be5968428
Liam Brown <liamnb525@gmail.com>
2021-03-05 03:46:13 -0500

    f

--- events/lib/events_web/router.ex
+++ events/lib/events_web/router.ex
@@ -7,7 +7,7 @@ defmodule EventsWeb.Router do
     plug :fetch_flash
     plug :protect_from_forgery
     plug :put_secure_browser_headers
-    plug PhotoBlogWeb.Plugs.FetchSession
+    plug EventsWeb.Plugs.FetchSession
   end
 
   pipeline :api do
